02194c8dde4ad3864997cc2a4d7746e8acccc970
mansour <mansour.bounab@etu.univ-smb.fr>
2025-12-10 11:36:34 +0100

    PARAMETRE

--- CreaGlace/Options.xaml.cs
+++ CreaGlace/Options.xaml.cs
@@ -22,6 +22,28 @@ namespace CreaGlace
         public Options()
         {
             InitializeComponent();
+            BoutonRetour.Click += BoutonRetour_Click;
+            BoutonSauvegarder.Click += BoutonSauvegarder_Click;
+            //"Quand on clique sur ce bouton, lance cette fonction"
+        }
+        private void BoutonRetour_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();// Ferme la fenêtre
+        }
+
+        private void BoutonSauvegarder_Click(object sender, RoutedEventArgs e)
+        {
+            // On récupère les valeurs de tes sliders (j'utilise tes noms exacts)
+            double musique = SliderMusique.Value;
+            double sfx = SlidersSFX.Value;
+
+            // Message de confirmation (tu pourras l'enlever plus tard)
+            string message = $"C'est noté !\nMusique : {musique:0}%\nSons : {sfx:0}%";
+
+            MessageBox.Show(message, "Options Glace", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // Optionnel : Ferme la fenêtre après la sauvegarde
+            this.Close();
         }
     }
 }
