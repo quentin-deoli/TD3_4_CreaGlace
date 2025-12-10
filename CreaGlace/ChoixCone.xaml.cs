edbf586f9c1b92ed9a0a5b9db105a8dbc118c2fb
Quentin De-Oliveira <Quentin.De-Oliveira@etu.univ-smb.fr>
2025-12-10 13:54:21 +0100

    PAGE GAME/FIN CHOIX CONE

--- CreaGlace/ChoixCone.xaml.cs
+++ CreaGlace/ChoixCone.xaml.cs
@@ -1,27 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace CreaGlace
 {
-    /// <summary>
-    /// Logique d'interaction pour ChoixCone.xaml
-    /// </summary>
     public partial class ChoixCone : Window
     {
+        private Image selectedImage = null;
+
         public ChoixCone()
         {
             InitializeComponent();
+        }
+
+        private void Cone_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            Image img = btn.Content as Image;
+
+            // Reset l'ancien cône si différent
+            if (selectedImage != null && selectedImage != img)
+            {
+                AnimateToNormal(selectedImage);
+            }
+
+            selectedImage = img;
+
+            // Agrandir le cône sélectionné
+            AnimateToBig(img);
+        }
+
+        private void AnimateToBig(Image img)
+        {
+            ScaleTransform scale = img.RenderTransform as ScaleTransform;
+
+            DoubleAnimation anim = new DoubleAnimation()
+            {
+                To = 1.3,
+                Duration = TimeSpan.FromMilliseconds(150),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            scale.BeginAnimation(ScaleTransform.ScaleXProperty, anim);
+            scale.BeginAnimation(ScaleTransform.ScaleYProperty, anim);
+        }
+
+        private void AnimateToNormal(Image img)
+        {
+            ScaleTransform scale = img.RenderTransform as ScaleTransform;
+
+            DoubleAnimation anim = new DoubleAnimation()
+            {
+                To = 1,
+                Duration = TimeSpan.FromMilliseconds(150),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            scale.BeginAnimation(ScaleTransform.ScaleXProperty, anim);
+            scale.BeginAnimation(ScaleTransform.ScaleYProperty, anim);
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedImage == null)
+            {
+                MessageBox.Show("Choisis un cône avant de continuer !");
+                return;
+            }
+
+            // Passer directement le ImageSource au jeu
+            ImageSource selectedConeSource = selectedImage.Source;
+
+            Game game = new Game(selectedConeSource);
+            game.Show();
+            this.Close();
         }
     }
 }
---
+++ CreaGlace/Game.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CreaGlace
+{
+    public partial class Game : Window
+    {
+        private double coneSpeed = 10; // Vitesse de déplacement
+
+        public Game(ImageSource coneImage)
+        {
+            InitializeComponent();
+
+            // Position initiale en bas au centre
+            imgConeChoisi.Source = coneImage;
+            double startX = (canvasJeu.ActualWidth - imgConeChoisi.Width) / 2;
+            double startY = canvasJeu.ActualHeight - imgConeChoisi.Height;
+            Canvas.SetLeft(imgConeChoisi, startX);
+            Canvas.SetTop(imgConeChoisi, startY);
+
+            // Focus pour capturer les touches
+            this.Loaded += (s, e) => this.Focus();
+            this.KeyDown += Game_KeyDown;
+
+            // Redimensionner le cône si la fenêtre change
+            canvasJeu.SizeChanged += CanvasJeu_SizeChanged;
+        }
+
+        private void CanvasJeu_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Repositionner le cône en bas si la fenêtre change
+            double currentX = Canvas.GetLeft(imgConeChoisi);
+            double currentY = Canvas.GetTop(imgConeChoisi);
+            Canvas.SetTop(imgConeChoisi, canvasJeu.ActualHeight - imgConeChoisi.Height);
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            double x = Canvas.GetLeft(imgConeChoisi);
+
+            if (e.Key == Key.Left)
+            {
+                x -= coneSpeed;
+                if (x < 0) x = 0; // Bord gauche
+            }
+            else if (e.Key == Key.Right)
+            {
+                x += coneSpeed;
+                if (x > canvasJeu.ActualWidth - imgConeChoisi.Width)
+                    x = canvasJeu.ActualWidth - imgConeChoisi.Width; // Bord droit
+            }
+
+            Canvas.SetLeft(imgConeChoisi, x);
+        }
+    }
+}
